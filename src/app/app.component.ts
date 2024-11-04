1ef6931984905c10331f0919198a1438e4961ad4
pankaj485 <pankajsamar2@gmail.com>
2024-11-04 23:21:20 +0530

    add primeng, primeng icons, configure it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule,
+    InputTextModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  constructor(private primengConfig: PrimeNGConfig) {}
+
   title = 'CSV2JSON-A18';
+  items: MenuItem[] = [
+    {
+      label: 'Update',
+      icon: 'pi pi-refresh',
+    },
+    {
+      label: 'Delete',
+      icon: 'pi pi-times',
+    },
+  ];
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    this.primengConfig.inputStyle.set('outlined');
+  }
 }
