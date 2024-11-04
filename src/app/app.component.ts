7e34ee1168fdf0fe905a5f91126d02db6527eaa9
pankaj485 <pankajsamar2@gmail.com>
2024-11-05 00:33:38 +0530

    add file upload component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuItem, PrimeNGConfig } from 'primeng/api';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { FileUploadHandlerEvent, FileUploadModule } from 'primeng/fileupload';
 import { InputTextModule } from 'primeng/inputtext';
 import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
@@ -17,27 +19,42 @@ import { ToolbarModule } from 'primeng/toolbar';
     ButtonModule,
     SplitButtonModule,
     InputTextModule,
+    ToastModule,
+    FileUploadModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  providers: [MessageService],
 })
 export class AppComponent {
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    private messageService: MessageService
+  ) {}
 
   title = 'CSV2JSON-A18';
-  items: MenuItem[] = [
-    {
-      label: 'Update',
-      icon: 'pi pi-refresh',
-    },
-    {
-      label: 'Delete',
-      icon: 'pi pi-times',
-    },
-  ];
+  uploadedFile?: File;
+  maxFileSize: number = 1000000;
 
   ngOnInit() {
     this.primengConfig.ripple = true;
     this.primengConfig.inputStyle.set('outlined');
+  }
+
+  uploadHandler(event: FileUploadHandlerEvent) {
+    const file = event.files[0];
+    if (file && file.size <= this.maxFileSize) {
+      this.uploadedFile = file;
+      this.messageService.add({
+        severity: 'info',
+        summary: 'File Added',
+        detail: file.name,
+        life: 1500,
+      });
+    }
+  }
+
+  onImageError(event: Event) {
+    console.log(event);
   }
 }
