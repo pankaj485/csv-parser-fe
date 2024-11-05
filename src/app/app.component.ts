45e6bdc5c277edefd12c27d9866c1e36ebefbf1c
pankaj485 <pankajsamar2@gmail.com>
2024-11-05 16:36:38 +0530

    organize imports for app component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import hljs from 'highlight.js/lib/core';
-import json from 'highlight.js/lib/languages/json';
-import 'highlight.js/styles/atom-one-dark.css';
+import { Component } from '@angular/core';
 import {
   ConfirmationService,
   MessageService,
   PrimeNGConfig,
 } from 'primeng/api';
-import { ButtonModule } from 'primeng/button';
-import { CheckboxChangeEvent, CheckboxModule } from 'primeng/checkbox';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { DividerModule } from 'primeng/divider';
-import { FieldsetModule } from 'primeng/fieldset';
-import { FileUploadHandlerEvent, FileUploadModule } from 'primeng/fileupload';
-import { PanelModule } from 'primeng/panel';
-import { ScrollPanelModule } from 'primeng/scrollpanel';
-import { ToastModule } from 'primeng/toast';
-import { ToolbarModule } from 'primeng/toolbar';
-import { sampleJsonData } from '../../db/samplejson';
-import { AccordionModule } from 'primeng/accordion';
-import { HeaderComponent } from './header/header.component';
 import { CodeBlockComponent } from './code-block/code-block.component';
-import { FileUploadComponent } from './file-upload/file-upload.component';
 import { FileHeadersComponent } from './file-headers/file-headers.component';
-hljs.registerLanguage('json', json);
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule,
-    FormsModule,
-    RouterOutlet,
-    ToolbarModule,
-    ButtonModule,
-    ToastModule,
-    FileUploadModule,
-    ConfirmDialogModule,
-    CheckboxModule,
-    PanelModule,
-    ScrollPanelModule,
-    DividerModule,
-    FieldsetModule,
-    AccordionModule,
-
     HeaderComponent,
     CodeBlockComponent,
     FileUploadComponent,
@@ -57,43 +23,11 @@ hljs.registerLanguage('json', json);
   providers: [MessageService, ConfirmationService],
 })
 export class AppComponent {
-  constructor(
-    private primengConfig: PrimeNGConfig,
-    private confirmationService: ConfirmationService,
-    private messageService: MessageService
-  ) {}
-  @ViewChild('codeBlock') codeBlockElement!: ElementRef;
-  @ViewChild('fileUploadBlock') fileUploadElement!: ElementRef;
-
+  constructor(private primengConfig: PrimeNGConfig) {}
   title = 'CSV2JSON-A18';
-  uploadedFile?: File;
-  // maxFileSize: number = 1000000;
-  uploadURL = '';
-  messageLife: number = 1500;
-  fileHeaders: string[] = Array(5)
-    .fill('Header ')
-    .map((item, i) => item + (i + 1));
-  selectedFileHeaders: string[] = [];
-  selectAll: boolean = false;
-  collapseFileUploadFieldSet: boolean = false;
-  collapseFileHeadersFieldSet: boolean = true;
-  collapseCodeBlockFieldSet: boolean = true;
 
   ngOnInit() {
     this.primengConfig.ripple = true;
     this.primengConfig.inputStyle.set('outlined');
-  }
-
-  getFileHeaders() {
-    this.collapseFileUploadFieldSet = true;
-    this.collapseFileHeadersFieldSet = false;
-
-    this.fileHeaders = Array(50)
-      .fill('Header 123')
-      .map((_, i) => _ + (i + 1));
-  }
-
-  onImageError(event: Event) {
-    console.log(event);
   }
 }
