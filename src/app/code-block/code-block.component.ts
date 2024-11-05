434f08dc617f3dee51fb3e132783934ca2f1781d
pankaj485 <pankajsamar2@gmail.com>
2024-11-05 15:38:02 +0530

    create standalone components

---
+++ src/app/code-block/code-block.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './code-block.component.html',
+  styleUrl: './code-block.component.css'
+})
+export class CodeBlockComponent {
+
+}
---
+++ src/app/file-headers/file-headers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-headers',
+  standalone: true,
+  imports: [],
+  templateUrl: './file-headers.component.html',
+  styleUrl: './file-headers.component.css'
+})
+export class FileHeadersComponent {
+
+}
