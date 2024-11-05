2a817f13e42641e704b127ed6d4dd6babd1aa804
pankaj485 <pankajsamar2@gmail.com>
2024-11-05 09:32:17 +0530

    implement file upload confirmation dialouge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import {
   PrimeNGConfig,
 } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { FileUploadHandlerEvent, FileUploadModule } from 'primeng/fileupload';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -21,18 +22,24 @@ import { ToolbarModule } from 'primeng/toolbar';
     ButtonModule,
     ToastModule,
     FileUploadModule,
+    ConfirmDialogModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [MessageService, ConfirmationService],
 })
 export class AppComponent {
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
+  ) {}
 
   title = 'CSV2JSON-A18';
   uploadedFile?: File;
   maxFileSize: number = 1000000;
   uploadURL = '';
+  messageLife: number = 1500;
 
   ngOnInit() {
     this.primengConfig.ripple = true;
@@ -45,8 +52,40 @@ export class AppComponent {
     }
   }
 
+  triggerFileUploadConfirmation(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon: 'none',
+      rejectIcon: 'none',
+      rejectButtonStyleClass: 'p-button-text',
+      accept: () => {
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Confirmed',
+          detail: 'Getting file headers',
+          life: this.messageLife,
+        });
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Rejected',
+          detail: 'File parsing aborted',
+          life: this.messageLife,
+        });
+      },
+    });
+  }
+
   getFileHeaders(file: File) {
     console.log(file);
+    const btn = <HTMLButtonElement>document.getElementById('confirmBtn');
+    const event = new Event('click');
+    btn.dispatchEvent(event);
+    this.triggerFileUploadConfirmation(event);
   }
 
   onImageError(event: Event) {
