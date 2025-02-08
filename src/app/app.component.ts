f82be6dc46dfca7b3fc983d15aa8b6e23b6d3851
pankaj485 <pankajsamar2@gmail.com>
2025-02-08 19:12:01 +0530

    feat: move file parser related components to dedicated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,17 @@
 import { Component } from '@angular/core';
 import {
   ConfirmationService,
-  Message,
   MessageService,
   PrimeNGConfig,
 } from 'primeng/api';
-import { Messages, MessagesModule } from 'primeng/messages';
-import { CodeBlockComponent } from './code-block/code-block.component';
-import { FileHeadersComponent } from './file-headers/file-headers.component';
-import { FileUploadComponent } from './file-upload/file-upload.component';
 import { HeaderComponent } from './header/header.component';
 import { StatesService } from './services/states.service';
+import { FileParserComponent } from './file-parser/file-parser.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    CodeBlockComponent,
-    FileUploadComponent,
-    FileHeadersComponent,
-    MessagesModule,
-  ],
+  imports: [HeaderComponent, FileParserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [MessageService, ConfirmationService],
@@ -31,20 +21,6 @@ export class AppComponent {
     private primengConfig: PrimeNGConfig,
     public statesService: StatesService
   ) {}
-
-  fileHeadersLoadingMessage: Message[] = [
-    {
-      detail: 'Getting File Headers',
-      severity: 'info',
-    },
-  ];
-
-  parsedDataLoadingMessage: Message[] = [
-    {
-      detail: 'Getting Parsed Data',
-      severity: 'info',
-    },
-  ];
 
   ngOnInit() {
     this.primengConfig.ripple = true;
---
+++ src/app/file-parser/file-parser.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import { FileUploadComponent } from '../file-upload/file-upload.component';
+import { FileHeadersComponent } from '../file-headers/file-headers.component';
+import { CodeBlockComponent } from '../code-block/code-block.component';
+import { StatesService } from '../services/states.service';
+import { Message } from 'primeng/api';
+import { MessagesModule } from 'primeng/messages';
+
+@Component({
+  selector: 'app-file-parser',
+  standalone: true,
+  imports: [
+    FileUploadComponent,
+    FileHeadersComponent,
+    CodeBlockComponent,
+    MessagesModule,
+  ],
+  templateUrl: './file-parser.component.html',
+  styleUrl: './file-parser.component.css',
+})
+export class FileParserComponent {
+  statesService = inject(StatesService);
+
+  fileHeadersLoadingMessage: Message[] = [
+    {
+      detail: 'Getting File Headers',
+      severity: 'info',
+    },
+  ];
+
+  parsedDataLoadingMessage: Message[] = [
+    {
+      detail: 'Getting Parsed Data',
+      severity: 'info',
+    },
+  ];
+}
